2376ddc3ed4db327bac32415bd2f158601a89923
BastianHarttung <info@harttung-fotografie.de>
2024-01-24 22:30:53 +0100

    feat(canvas-confetti): confetti on win

--- src/components/ModalWin.tsx
+++ src/components/ModalWin.tsx
@@ -2,6 +2,8 @@ import { Box, Fade, Modal, Typography } from "@mui/material";
 import { useGameStore } from "../store/game-store.ts";
 import { useShallow } from "zustand/react/shallow";
 import LeseLogo from "../assets/Leseteppich_Logo.svg";
+import confetti from "canvas-confetti"
+import { useEffect } from "react";
 
 
 const style = {
@@ -29,6 +31,45 @@ const ModalWin = () => {
       {initialTimeInSeconds: state.initialTimeInSeconds}
     ))
   )
+
+  useEffect(() => {
+    if (isWinModalOpen) {
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: {y: 0.6},
+        zIndex: 9999
+      })
+      const duration = 8 * 1000;
+      const animationEnd = Date.now() + duration;
+      const defaults = {startVelocity: 30, spread: 360, ticks: 60, zIndex: 9999};
+
+      const randomInRange = (min: number, max: number) => {
+        return Math.random() * (max - min) + min;
+      }
+
+      const interval = setInterval(function () {
+        const timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+          return clearInterval(interval);
+        }
+
+        const particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        confetti({
+          ...defaults,
+          particleCount,
+          origin: {x: randomInRange(0.1, 0.3), y: Math.random() - 0.2}
+        });
+        confetti({
+          ...defaults,
+          particleCount,
+          origin: {x: randomInRange(0.7, 0.9), y: Math.random() - 0.2}
+        });
+      }, 250);
+    }
+  }, [isWinModalOpen]);
 
 
   return (
