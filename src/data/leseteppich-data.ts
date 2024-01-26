1fc78f1628926545d64c3227abbace4a119152bc
BastianHarttung <info@harttung-fotografie.de>
2024-01-26 19:06:23 +0100

    feat(leseteppich): add leseteppich 14 to data and image

--- src/data/leseteppich-data.ts
+++ src/data/leseteppich-data.ts
@@ -24,8 +24,8 @@ export const leseteppiche: Leseteppich[] = [
       "bitte", "aber", "Bein", "toben", "Bluse",
       "Blume", "Bonbon", "Besen", "Birne", "Ball",
       "Biber", "Ballett", "Brombeere", "Banane", "basteln",
-      "Mama mag bunte Blumen",
-      "Piri und Leo blubbern mit Limo"
+      "Mama mag bunte Blumen.",
+      "Piri und Leo blubbern mit Limo."
     ]
   },
   {
@@ -39,6 +39,19 @@ export const leseteppiche: Leseteppich[] = [
       "Locher", "rechnen", "machen", "lachen", "suchen",
       "Leon sucht sein Buch unter dem Tuch.",
       "Die Buben basteln nachts."
+    ]
+  },
+  {
+    id: 14,
+    chars: ["M", "A", "L", "E", "O", "R", "I", "T", "U", "N", "S", "Ei", "H", "B", "-ch", "F"],
+    strings: [
+      "Foto", "Farbe", "Film", "Fest", "Ofen",
+      "Ufo", "Affe", "Seife", "Saft", "Tafel",
+      "Telefon", "Elefant", "Elfe", "Fenster", "Film",
+      "fressen", "haben", "fallen", "toben", "Teich",
+      "Futter", "helfen", "rufen", "holen", "filmen",
+      "Anna malt einen Roboter.",
+      "Oma filmt einen Affen."
     ]
   },
 ]
