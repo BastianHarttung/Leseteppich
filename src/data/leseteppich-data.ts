66779713b1954ff29bbe5ce5ea1f5f61760375e3
BastianHarttung <info@harttung-fotografie.de>
2024-01-27 21:39:13 +0100

    data(leseteppich-data): add leseteppich 10 + 11

--- src/data/leseteppich-data.ts
+++ src/data/leseteppich-data.ts
@@ -6,41 +6,41 @@ export const leseteppiche: Leseteppich[] = [
   //   chars: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "Ei", "Sch"],
   //   strings: [
   //     "null0", "eins1", "zwei2",
-      // "drei3", "vier4",
-      // "fünf5", "sechs6", "sieben7", "acht8", "neun9",
-      // "zehn10", "elf11", "zwölf12", "dreizehn13", "vierzehn14",
-      // "fünfzehn15", "sechszehn16", "siebzehn17", "achtzehn18", "neunzehn19",
-      // "zwanzig20", "einundzwanzig21", "zweiundzwanzig22", "dreiundzwanzig23", "vierundzwanzig24",
-      // "Erster langer Satz wahrscheinlich mit Umbruch.",
-      // "Zweiter etwas kürzerer Satz."
+  // "drei3", "vier4",
+  // "fünf5", "sechs6", "sieben7", "acht8", "neun9",
+  // "zehn10", "elf11", "zwölf12", "dreizehn13", "vierzehn14",
+  // "fünfzehn15", "sechszehn16", "siebzehn17", "achtzehn18", "neunzehn19",
+  // "zwanzig20", "einundzwanzig21", "zweiundzwanzig22", "dreiundzwanzig23", "vierundzwanzig24",
+  // "Erster langer Satz wahrscheinlich mit Umbruch.",
+  // "Zweiter etwas kürzerer Satz."
   //   ]
   // },
-  // {
-  //   id: 10,
-  //   chars: ["M", "A", "L", "E", "O", "R", "I", "T", "U", "N", "S", "Ei"],
-  //   strings: [
-  //     "ein", "eine", "um", "am", "alle",
-  //     "Bus", "bald", "bin", "bis", "bei",
-  //     "bitte", "aber", "Bein", "toben", "Bluse",
-  //     "Blume", "Bonbon", "Besen", "Birne", "Ball",
-  //     "Biber", "Ballett", "Brombeere", "Banane", "basteln",
-  //     "Mama mag bunte Blumen.",
-  //     "Piri und Leo blubbern mit Limo."
-  //   ]
-  // },
-  // {
-  //   id: 11,
-  //   chars: ["M", "A", "L", "E", "O", "R", "I", "T", "U", "N", "S", "Ei", "H"],
-  //   strings: [
-  //     "ein", "eine", "um", "am", "alle",
-  //     "Bus", "bald", "bin", "bis", "bei",
-  //     "bitte", "aber", "Bein", "toben", "Bluse",
-  //     "Blume", "Bonbon", "Besen", "Birne", "Ball",
-  //     "Biber", "Ballett", "Brombeere", "Banane", "basteln",
-  //     "Mama mag bunte Blumen.",
-  //     "Piri und Leo blubbern mit Limo."
-  //   ]
-  // },
+  {
+    id: 10,
+    chars: ["M", "A", "L", "E", "O", "R", "I", "T", "U", "N", "S", "Ei"],
+    strings: [
+      "no", "na", "on", "ein", "nein",
+      "mein", "rein", "sein", "eine", "nass",
+      "Nase", "Eimer", "Meise", "Tonne", "Sonne",
+      "Tor", "Torte", "Sessel", "Semmel", "Reis",
+      "Eis", "essen", "meinen", "alleine", "Eier",
+      "Eimer", "Seil", "Ameise", "Amsel", "Lisa",
+      "teilen", "eine", "Leiter", "Nuss", "leise"
+    ]
+  },
+  {
+    id: 11,
+    chars: ["M", "A", "L", "E", "O", "R", "I", "T", "U", "N", "S", "Ei", "H"],
+    strings: [
+      "ha", "ho", "hi", "he", "hu",
+      "ni", "nu", "ne", "na", "no",
+      "ein", "sein", "mein", "her", "hin",
+      "Hanna", "Rehe", "Hase", "Hose", "mir",
+      "sehen", "Hammer", "Halm", "Hut", "Huhn",
+      "Eimer", "holen", "Hahn", "Uhu", "Atlas",
+      "Nashorn", "Einhorn", "Himmel", "Helm", "Malerin"
+    ]
+  },
   {
     id: 12,
     chars: ["M", "A", "L", "E", "O", "R", "I", "T", "U", "N", "S", "Ei", "H", "B"],
