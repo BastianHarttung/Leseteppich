b0de42b46d391d8a87449b579d18cc61e31f8d25
BastianHarttung <info@harttung-fotografie.de>
2024-02-11 17:20:48 +0100

    feat(Imprint): add text to Imprint

--- src/pages/Imprint/Imprint.tsx
+++ src/pages/Imprint/Imprint.tsx
@@ -12,10 +12,26 @@ const Imprint = () => {
     <main style={{justifyContent: "flex-start"}}>
       <LeseteppichAppBar/>
 
-      <Box display={"flex"} flexDirection={"column"} gap={2} py={10}>
+      <Box display={"flex"} flexDirection={"column"} gap={2} pt={10} pb={2} px={2} textAlign={"left"}>
         <Typography variant={"h6"}>Impressum</Typography>
-        <Typography variant={"body1"}>Dies ist ein ausschließlich privates Projekt von Bastian Harttung ohne
-          kommerzielle Absichten.</Typography>
+        <Typography variant={"body1"}>
+          Dies ist ein ausschließlich privates Projekt von mir, <b>Bastian Harttung</b> ohne kommerzielle
+          Absichten. <br/>
+          Ich bin nur ein Papa, der seinem Sohn einen Anreiz geben wollte, um schneller und mit mehr Spaß zu lesen und
+          seine Übungen des Leseteppichs zu genießen. Um auch anderen Eltern zu helfen, dass ihre Kinder Spaß beim Lesen
+          haben, habe ich das Projekt geteilt.<br/>
+          Also viel Spaß mit meiner kleinen App.
+        </Typography>
+
+        <Typography variant={"body1"}>
+          Bei Fragen oder Anregungen kannst du mich über die Email: <a
+          href="mailto:info@bastian-harttung.de">info@bastian-harttung.de</a> kontaktieren.
+        </Typography>
+
+        <Typography variant={"body1"}>
+          Diese Web-App speichert keine Daten im Hintergrund oder erfasst Nutzungsdaten. Nur die Highscores des
+          Benutzers werden auf dem jeweiligen Gerät im localStorage gespeichert.
+        </Typography>
       </Box>
     </main>
   );
